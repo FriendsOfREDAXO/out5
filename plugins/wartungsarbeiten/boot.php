5845812fb6493d11a00cbb0b5090b03a742c2eb4
Thomas Skerbis <skerbis@klxm.de>
2017-04-28 16:21:41 +0200

    Adds IP-Check

--- plugins/wartungsarbeiten/boot.php
+++ plugins/wartungsarbeiten/boot.php
@@ -2,9 +2,11 @@
 if (!rex::isBackend()) {
 	if ($this->getConfig('status') != 'deaktiviert') {
 		$session = rex_backend_login::hasSession();
-		if (!$session) {
+		$ipcheck = $_SERVER['REMOTE_ADDR'];
+		if (!$session and $ipcheck!=$this->getConfig('ip')) {
 			header('Location: ' . $this->getConfig('url'));
 			exit;
   		}
   	}
 }
+
