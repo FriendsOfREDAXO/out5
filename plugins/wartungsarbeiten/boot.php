d1da8834f3e16b374e46ee30d9aff58daab8662e
Thomas Skerbis <thomas.skerbis@mac.com>
2017-08-28 23:54:17 +0200

    Update boot.php

--- plugins/wartungsarbeiten/boot.php
+++ plugins/wartungsarbeiten/boot.php
@@ -2,7 +2,7 @@
 if (!rex::isBackend()) {
 	if ($this->getConfig('status') != 'deaktiviert') {
 		$session = rex_backend_login::hasSession();
-		if (!$session and $this->getConfig('ip')!=$_SERVER['REMOTE_ADDR']) {
+		if (!$session or $this->getConfig('ip')!=$_SERVER['REMOTE_ADDR']) {
 			header('Location: ' . $this->getConfig('url'));
 			exit;
   		}
